b1f7e4932fcc5afd23dcdf544b91d278d8ccc797
Mohsen Rad <irregular.celery@gmail.com>
2025-01-24 21:55:57 +0330

    Module `dev`: Test `celui_math` items

--- modules/dev/src/main.rs
+++ modules/dev/src/main.rs
@@ -1,4 +1,15 @@
+use celui::{Color, Mat2, Mat3, Mat4, Rect, Vec2, Vec3, Vec4};
+
 fn main() {
-    println!("Hello, world!");
-    println!("{}", celui::add(50, 15));
+    println!("{:?}", Vec2::new(1.0, 2.0));
+    println!("{:?}", Vec3::new(1.0, 2.0, 3.0));
+    println!("{:?}", Vec4::new(1.0, 2.0, 3.0, 4.0));
+
+    println!("{:?}", Mat2::identity());
+    println!("{:?}", Mat3::identity());
+    println!("{:?}", Mat4::identity());
+
+    println!("{:?}", Rect::new(0.0, 0.0, 32.0, 32.0));
+
+    println!("{:?}", Color::BLACK);
 }
