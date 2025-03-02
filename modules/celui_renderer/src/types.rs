7154a95e9f164c84fbff3aad34dc174871b05a19
Mohsen Rad <irregular.celery@gmail.com>
2025-03-02 20:47:49 +0330

    Module `celui_renderer`: Create `TextureRegistry` struct

--- modules/celui_renderer/src/types.rs
+++ modules/celui_renderer/src/types.rs
@@ -17,5 +17,27 @@ pub(crate) struct Texture {
     id: TextureId,
 }
 
+pub(crate) struct TextureRegistry {
+    /// Default texture used for colored primitives.
+    default: Texture,
+    /// Texture slots, stores the backend id for the textures.
+    slots: Vec<u32>,
+    /// Next available texture slot.
+    index: usize,
+    /// Lookup map of the textures.
+    ///
+    /// **Key:** Backend texture id.
+    /// **Value:** Texture slot index.
+    map: std::collections::HashMap<u32, usize>, // TODO: USE `celui_collections::HashMap`
+}
+
+impl TextureRegistry {
+    #[inline(always)]
+    fn reset(&mut self) {
+        self.index = 1; // `0` is reserved for the default texture
+        self.map.clear();
+    }
+}
+
 // TODO: Move this to the backend crate
 pub trait GraphicsBackend {}
